3ebdb3bff7c4c0674d72481aee92808c2f120e95
tec234 <thomastec234@gmail.com>
2026-02-11 20:17:03 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     [Tooltip("Movement speed in units/second")]
     public float moveSpeed = 5f;
-
+#ur gay
     Rigidbody2D rb;
     Vector2 movement;
 
