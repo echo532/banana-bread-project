dfa5cc5dbc7593776440c44e72c1222d25179bf9
Nicholas Heineman <nheineman@comcast.net>
2026-03-03 18:06:57 -0500

    full project restructure

--- Assets/Editor/LoadScript.cs
+++ Assets/Editor/LoadScript.cs
@@ -11,12 +11,7 @@ public static class LoadScript
     [MenuItem("Tools/Build Ojects")]
     static void LoadAllScenes()
     {
-        //Menu Scene
-        MenuSetup.GenerateMenu();
-        //Scene 1
-        AutoUISetup.RunOnce();
-        AutoPlayerSetup.RunOnce();
-        AutoEnemySetup.RunOnce();
+        
 
         EditorApplication.delayCall += () =>
         {
